7f5ecb9790edfc30ec26d6f22a641139cb2991be
rockybhaibolthey <156048911+rockybhaibolthey@users.noreply.github.com>
2025-11-26 07:42:53 +0530

    Update card_gif.dart

--- lib/widgets/card_gif.dart
+++ lib/widgets/card_gif.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 class HalloweenCard extends StatelessWidget {
   const HalloweenCard({super.key});
-
+ 
 @override
 Widget build(BuildContext context) {
   return InkWell(
